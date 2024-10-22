d9a50adf19a7ff415c4bbca35baae2faaf3dd692
racheltoto <racheltt.hk@gmail.com>
2024-10-22 17:31:55 +1100

    modified the RequestLogin.kt

--- app/src/main/java/com/example/lab2_s4670360/data/RequestLogin.kt
+++ app/src/main/java/com/example/lab2_s4670360/data/RequestLogin.kt
@@ -3,6 +3,7 @@ package com.example.lab2_s4670360.data
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+// This data class is typically used when sending login information to a server or receiving it.
 @JsonClass(generateAdapter = true)
 data class RequestLogin(
     @Json(name = "username") val username: String,
