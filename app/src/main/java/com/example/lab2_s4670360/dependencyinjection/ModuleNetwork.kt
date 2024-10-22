59d99b839aa4b52f1efe98bb5c91dc1263c49f6a
racheltoto <racheltt.hk@gmail.com>
2024-10-22 17:44:30 +1100

    modified the ModuleNetwork.kt

--- app/src/main/java/com/example/lab2_s4670360/dependencyinjection/ModuleNetwork.kt
+++ app/src/main/java/com/example/lab2_s4670360/dependencyinjection/ModuleNetwork.kt
@@ -8,15 +8,20 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-
+//Marks this class as a Dagger module, which is used to provide dependencies.
 @Module
+
+//- Specifies that this module is installed in the `SingletonComponent`, meaning the provided dependencies
+//      will have a singleton lifecycle
 @InstallIn(SingletonComponent::class)
 
+//Declares a Kotlin `object`, which ensures that only one instance of this module exists at runtime.
 object ModuleNetwork{
     @Provides
 
     @Singleton
-
+//This function returns an instance of `ApiService`. It uses `ApiRetrofitClient.apiService` to get the actual
+//      implementation of the `ApiService`. Dagger will call this method when the `ApiService` is required.
     fun provideApiService(): ApiService {
 
         return ApiRetrofitClient.apiService
