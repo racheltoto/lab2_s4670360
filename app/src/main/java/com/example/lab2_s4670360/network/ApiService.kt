dc4b2f85270cfa413a39100258cf9484241d85a3
racheltoto <racheltt.hk@gmail.com>
2024-10-22 18:04:17 +1100

    modified the ApiService

--- app/src/main/java/com/example/lab2_s4670360/network/ApiService.kt
+++ app/src/main/java/com/example/lab2_s4670360/network/ApiService.kt
@@ -8,13 +8,14 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
+//The ApiService interface contains methods for making HTTP requests to the server.
 interface ApiService {
 
-
+//Sends user login details (username and password) to the server using a POST request.
     @POST("footscray/auth")
     suspend fun login(@Body request: RequestLogin): ResponseLogin
 
-
+//Retrieves historical data from the server using a GET request
     @GET("dashboard/history")
     suspend fun getHistory(
         @Header("Authorization") keypass: String
